6e0e7a4ada951e7594191964dc8271548a56c074
John.MP3 <juanramonmejiapadilla@gmail.com>
2025-05-21 00:31:12 -0600

    Cambios minimos a los codigos
    Se cambio el layout.tsx del dashboard debido a que se miraba
    el scroll bar horizontal. Se cambio el Menu para que se muestre
    el contenido de manera dinamica, es decir que dependiendo del
    usuario se van a mostrar diferentes elementos en el Navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -53,42 +54,42 @@ const menuItems = [
                 href: "/list/exams",
                 visible: ["admin", "teacher", "student", "parent"],
             },
-            // {
-            //     icon: "/assignment.png",
-            //     label: "Assignments",
-            //     href: "/list/assignments",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
-            // {
-            //     icon: "/result.png",
-            //     label: "Results",
-            //     href: "/list/results",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
-            // {
-            //     icon: "/attendance.png",
-            //     label: "Attendance",
-            //     href: "/list/attendance",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
-            // {
-            //     icon: "/calendar.png",
-            //     label: "Events",
-            //     href: "/list/events",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
-            // {
-            //     icon: "/message.png",
-            //     label: "Messages",
-            //     href: "/list/messages",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
-            // {
-            //     icon: "/announcement.png",
-            //     label: "Announcements",
-            //     href: "/list/announcements",
-            //     visible: ["admin", "teacher", "student", "parent"],
-            // },
+            {
+                icon: "/assignment.png",
+                label: "Assignments",
+                href: "/list/assignments",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+                icon: "/result.png",
+                label: "Results",
+                href: "/list/results",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+                icon: "/attendance.png",
+                label: "Attendance",
+                href: "/list/attendance",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+                icon: "/calendar.png",
+                label: "Events",
+                href: "/list/events",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+                icon: "/message.png",
+                label: "Messages",
+                href: "/list/messages",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
+            {
+                icon: "/announcement.png",
+                label: "Announcements",
+                href: "/list/announcements",
+                visible: ["admin", "teacher", "student", "parent"],
+            },
         ],
     },
     {
@@ -119,15 +120,25 @@ const menuItems = [
 const Menu = () => {
     return (
         <div className="mt-4 text-sm">
-            {menuItems.map((menu) => (
-                <div className="flex flex-col gap-2" key={menu.title}>
-                    <span className="hidden lg:block text-gray-400 font-light my-4">{menu.title}</span>
-                    {menu.items.map((item) => (
-                        <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2">
-                            <Image src={item.icon} alt="" width={20} height={20} />
-                            <span className="hidden lg:block">{item.label}</span>
-                        </Link>
-                    ))}
+            {menuItems.map((i) => (
+                <div className="flex flex-col gap-2" key={i.title}>
+                    <span className="hidden lg:block text-gray-400 font-light my-4">
+                        {i.title}
+                    </span>
+                    {i.items.map((item) => {
+                        if (item.visible.includes(role)) {
+                            return (
+                                <Link
+                                    href={item.href}
+                                    key={item.label}
+                                    className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-SkyLight"
+                                >
+                                    <Image src={item.icon} alt="" width={20} height={20} />
+                                    <span className="hidden lg:block">{item.label}</span>
+                                </Link>
+                            );
+                        }
+                    })}
                 </div>
             ))}
         </div>
