8a521b646100195b4848d35d88fbb54baa2fc9d7
John.MP3 <juanramonmejiapadilla@gmail.com>
2025-05-20 22:59:00 -0600

    Creacion del "AttendanceChart"
    Se implemento el componente de AttendanceChart el cual nos
    permitira ver la cantidad de personas atendidas por dia.

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import AttendanceChart from "@/components/AttendanceChard";
 import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
@@ -19,9 +20,18 @@ const AdminPage = () => {
                     <div className="w-full lg:w-1/3 h-[450px]">
                         <CountChart />
                     </div>
+                    {/* ATTENDANCE CHART */}
+                    <div className="w-full lg:w-2/3 h-[450px]">
+                        <AttendanceChart />
+                    </div>
+                </div>
+                {/* BOTTOM CHART */}
+                <div className="w-full h-[500px]">
                 </div>
             </div>
             {/* RIGHT */}
+            <div className="w-full lg:w-1/3 flex flex-col gap-8">
+            </div>
         </div>
     );
 };
