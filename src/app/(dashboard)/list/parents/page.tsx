5ede4442be9c4b01ae4a94d7a902a4fc9d7131b9
John.MP3 <juanramonmejiapadilla@gmail.com>
2025-05-21 22:41:53 -0600

    Creacion de las paginas de listas
    Se crearon las paginas de la seccion de listas donde se puede ver
    informacion mas detallada del alumno, maestro y padre. Tambien
    se pueden ver las actividades, examenes, anuncios, etc.

--- src/app/(dashboard)/list/parents/page.tsx
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -1,7 +1,7 @@
-// import FormModal from "@/components/FormModal";
-// import Pagination from "@/components/Pagination";
-// import Table from "@/components/Table";
-// import TableSearch from "@/components/TableSearch";
+import FormModal from "@/components/FormModal";
+import Pagination from "@/components/Pagination";
+import Table from "@/components/Table";
+import TableSearch from "@/components/TableSearch";
 import { parentsData, role } from "@/lib/data";
 import Image from "next/image";
 
@@ -59,8 +59,8 @@ const ParentListPage = () => {
                 <div className="flex items-center gap-2">
                     {role === "admin" && (
                         <>
-                            {/* <FormModal table="parent" type="update" data={item} />
-                            <FormModal table="parent" type="delete" id={item.id} /> */}
+                            <FormModal table="parent" type="update" data={item} />
+                            <FormModal table="parent" type="delete" id={item.id} />
                         </>
                     )}
                 </div>
@@ -74,7 +74,7 @@ const ParentListPage = () => {
             <div className="flex items-center justify-between">
                 <h1 className="hidden md:block text-lg font-semibold">All Parents</h1>
                 <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-                    {/* <TableSearch /> */}
+                    <TableSearch />
                     <div className="flex items-center gap-4 self-end">
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                             <Image src="/filter.png" alt="" width={14} height={14} />
@@ -82,16 +82,16 @@ const ParentListPage = () => {
                         <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
                             <Image src="/sort.png" alt="" width={14} height={14} />
                         </button>
-                        {/* {role === "admin" && (
+                        {role === "admin" && (
                             <FormModal table="teacher" type="create" />
-                        )} */}
+                        )}
                     </div>
                 </div>
             </div>
             {/* LIST */}
-            {/* <Table columns={columns} renderRow={renderRow} data={parentsData} /> */}
+            <Table columns={columns} renderRow={renderRow} data={parentsData} />
             {/* PAGINATION */}
-            {/* <Pagination /> */}
+            <Pagination />
         </div>
     );
 };
